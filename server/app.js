6cf77d476c6ed4cabc4033a4560d8fa06dece501
AE <austence@sbcglobal.net>
2019-03-29 11:41:56 -0700

    adds code to fix /logs key values.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const path = require('path');
 
 
 app.use((req, res, next) => {
@@ -11,9 +12,10 @@ app.use((req, res, next) => {
     var resource = req.url;
     var version = 'HTTP/' + req.httpVersion; 
     var status = res.statusCode;
-    var loglineData = `${userAgent},${time},${method},${resource},${version},${status}`; 
+    var loglineData = `\"${userAgent}\",\"${time}\",\"${method}\",\"${resource}\",\"${version}\",\"${status}\"`; 
 
-    fs.appendFile('log.csv', loglineData + '\n', (err) => {
+    console.log(__dirname);
+    fs.appendFile(path.join(__dirname, 'log.csv'), loglineData + '\n', (err) => {
         if (err) throw err;
         console.log('wrote to log.csv');
     });
@@ -23,39 +25,61 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
 // write your code to respond "ok" here
-    console.log();
     res.send('ok');
     res.status('200');
     return;
 });
 
+function csvJSON(csv){  //var csv is the CSV file with headers
+    console.log('csv = ' + csv);
+    var lines=csv.split("\n");
+    console.log(lines[0]);
+    console.log(lines[1]);
+    var result = [];
+    var headers=lines[0].split(",");
+    console.log(headers);
+    for(var i=1;i<lines.length;i++){
+        var obj = {};
+        var currentline=lines[i].splitCSV();
+        if (currentline.length < headers.length) {
+            continue;
+        }
+        console.log('currentline = ' + currentline);
+        console.log('currentline.length =' + currentline.length);
+        console.log('headers.length =' + headers.length);
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    console.log('csvJSON = ' + JSON.stringify(result));
+    return result; //JavaScript object
+    
+};
+
+String.prototype.splitCSV = function(sep) {
+    for (var foo = this.split(sep = sep || ","), x = foo.length - 1, tl; x >= 0; x--) {
+      if (foo[x].replace(/"\s+$/, '"').charAt(foo[x].length - 1) == '"') {
+        if ((tl = foo[x].replace(/^\s+"/, '"')).length > 1 && tl.charAt(0) == '"') {
+          foo[x] = foo[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
+        } else if (x) {
+          foo.splice(x - 1, 2, [foo[x - 1], foo[x]].join(sep));
+        } else foo = foo.shift().split(sep).concat(foo);
+      } else foo[x].replace(/""/g, '"');
+    } 
+    console.log('splitCSV = ' + foo);
+    console.log('splitCSV[3]' + foo[3]);
+    var a = ['a', 'b', 'c', 'd'];
+    console.log(a);
+    return foo;
+};
+
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
-    var csvLog = '../log.csv';
-    fs.readFile(csvLog, function read(err, data) {
-        if (err) {
-            throw err;
-        }
-        csvData = data;
-    });    
-    //var csv is the CSV file with headers
-        function csvJSON(csv){
+    var csvLog = path.join(__dirname, 'log.csv');
+    var csvData = fs.readFileSync(csvLog, 'utf8');  
         
-            var lines=csv.split("\n");
-            var result = [];
-            var headers=lines[0].split(",");
-            for(var i=1;i<lines.length;i++){
-                var obj = {};
-                var currentline=lines[i].split(",");
-                for(var j=0;j<headers.length;j++){
-                    obj[headers[j]] = currentline[j];
-                }
-                result.push(obj);
-            }
-            return result; //JavaScript object
-            //return JSON.stringify(result); //JSON
-        }
-        res.send(csvJSON);
+    res.send(csvJSON(csvData));
 });
 
 module.exports = app;
