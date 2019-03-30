4eb36811cfae1f5ac2dbe6acab22f9eed2110a4a
AE <austence@sbcglobal.net>
2019-03-30 16:52:51 -0700

    removes unnecesssary console.logs and comments from app.js.

--- server/app.js
+++ server/app.js
@@ -29,46 +29,26 @@ app.get('/', (req, res) => {
 });
 
 function csvJSON(csv){  //var csv is the CSV file with headers
-    console.log('csv = ' + csv);
     var lines=csv.split('\n');
     var result = [];
     var headers=lines[0].split(',');
     console.log(headers);
     for(var i=1;i<lines.length;i++){
         var obj = {};
-    //    var currentline=lines[i].splitCSV();
         var currentline = lines[i].split(',');
         if (currentline.length < headers.length) {
             continue;
         }
-    /*    console.log('currentline = ' + currentline);
-        console.log('currentline.length =' + currentline.length);
-        console.log('headers.length =' + headers.length);
-    */    
+
         for(var j=0;j<headers.length;j++){
             obj[headers[j]] = currentline[j];
         }
         result.push(obj);
     }
-    console.log('csvJSON = ' + JSON.stringify(result));
     return result; //JavaScript object
     
 };
-/*
-String.prototype.splitCSV = function(sep) {
-    for (var foo = this.split(sep = sep || ","), x = foo.length - 1, tl; x >= 0; x--) {
-      if (foo[x].replace(/"\s+$/, '"').charAt(foo[x].length - 1) == '"') {
-        if ((tl = foo[x].replace(/^\s+"/, '"')).length > 1 && tl.charAt(0) == '"') {
-          foo[x] = foo[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
-        } else if (x) {
-          foo.splice(x - 1, 2, [foo[x - 1], foo[x]].join(sep));
-        } else foo = foo.shift().split(sep).concat(foo);
-      } else foo[x].replace(/""/g, '"');
-    } 
-    console.log('splitCSV = ' + foo);
-    return foo;
-};
-*/
+
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
     var csvLog = path.join(__dirname, 'log.csv');
